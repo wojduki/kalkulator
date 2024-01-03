27c76e57854465128d99693626eaca38179ac609
wojduki <wojduki@gmail.com>
2024-01-03 20:32:00 +0100

    index added, controller refactor

--- src/main/java/com/wojduki/kalkulator/controller/KalkulatorController.java
+++ src/main/java/com/wojduki/kalkulator/controller/KalkulatorController.java
@@ -1,26 +1,14 @@
 package com.wojduki.kalkulator.controller;
 
-import com.wojduki.kalkulator.model.Material;
-import com.wojduki.kalkulator.repository.MaterialRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/kalkulator")
 public class KalkulatorController {
-    private final MaterialRepository materialRepository;
-    @Autowired
-    public KalkulatorController(MaterialRepository materialRepository) {
-        this.materialRepository = materialRepository;
-    }
 
-    @GetMapping
-    public List<Material> getMaterial() {
-        return materialRepository.findAll();
-    }
+    public KalkulatorController(){}
 
+    @GetMapping("/kalk")
+    public Double liczObjetosc (@RequestParam double dlugosc, @RequestParam double szerokosc, @RequestParam double wysokosc) {
+        return dlugosc*szerokosc*wysokosc;
+    }
 }
