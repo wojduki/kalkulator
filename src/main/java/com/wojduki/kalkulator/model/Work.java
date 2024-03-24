37ff2fa183a87bc861b175c3d2d1b58abc192bea
wojduki <wojduki@gmail.com>
2024-03-24 05:43:02 +0100

    before names

--- src/main/java/com/wojduki/kalkulator/model/Work.java
+++ src/main/java/com/wojduki/kalkulator/model/Work.java
@@ -3,17 +3,17 @@ package com.wojduki.kalkulator.model;
 import jakarta.persistence.*;
 
 @Entity
-public class Robocizna extends Cost {
+public class Work extends Cost {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    public Robocizna(String nazwa, double cenaZaM2, Rodzaj rodzaj) {
+    public Work(String nazwa, double cenaZaM2, Rodzaj rodzaj) {
         super(nazwa, cenaZaM2, rodzaj);
     }
 
-    public Robocizna() {
+    public Work() {
     }
 }
--- src/main/java/com/wojduki/kalkulator/repository/WorkRepository.java
+++ src/main/java/com/wojduki/kalkulator/repository/WorkRepository.java
@@ -1,8 +1,8 @@
 package com.wojduki.kalkulator.repository;
 
-import com.wojduki.kalkulator.model.Robocizna;
+import com.wojduki.kalkulator.model.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface RobociznaRepository extends JpaRepository<Robocizna, Long> {
+public interface WorkRepository extends JpaRepository<Work, Long> {
 }
