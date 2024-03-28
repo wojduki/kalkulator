ec4d3dcad42ecf99c049c188ede5b349e483f07a
wojduki <wojduki@gmail.com>
2024-03-28 11:14:27 +0100

    jquery added

--- src/main/java/com/wojduki/kalkulator/controllers/MaterialController.java
+++ src/main/java/com/wojduki/kalkulator/controllers/MaterialController.java
@@ -5,7 +5,11 @@ import com.wojduki.kalkulator.services.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @Controller
@@ -19,5 +23,9 @@ public class MaterialController {
         List<Material> materialList = materialService.getAllMaterials();
         model.addAttribute("materials", materialList);
         return "materials";
+    }
+    @RequestMapping("/newmaterial")
+    public void createNewMaterial(Material material) {
+        materialService.createMaterial(material);
     }
 }
--- src/main/java/com/wojduki/kalkulator/services/MaterialService.java
+++ src/main/java/com/wojduki/kalkulator/services/MaterialService.java
@@ -16,4 +16,8 @@ public class MaterialService {
     public List<Material> getAllMaterials() {
         return new ArrayList<>(materialRepo.getAllMaterials());
     }
+    public void createMaterial(Material material) {
+        materialRepo.createMaterial(material.getName(), material.getUnit(), material.getConsumptionPerM2(),
+                material.getUnitPrice(), material.getRodzaj());
+    }
 }
