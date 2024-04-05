010b54620de3996d9b993ba0ae61d55d983b2979
wojduki <wojduki@gmail.com>
2024-04-05 08:56:37 +0200

    change annotations in controller

--- src/main/java/com/wojduki/kalkulator/controllers/MaterialController.java
+++ src/main/java/com/wojduki/kalkulator/controllers/MaterialController.java
@@ -2,15 +2,10 @@ package com.wojduki.kalkulator.controllers;
 
 import com.wojduki.kalkulator.model.Material;
 import com.wojduki.kalkulator.services.MaterialService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
@@ -19,41 +14,41 @@ public class MaterialController {
     @Autowired
     MaterialService materialService;
 
-    @RequestMapping("/material/select")
+    @GetMapping("/material/select")
     public String presentMaterialsInSelect(Model model) {
         List<Material> materialList = materialService.getAllMaterials();
         model.addAttribute("materialList", materialList);
         return "materialselect";
     }
-    @RequestMapping("/materials")
+    @GetMapping("/materials")
     public String getAllMaterials(Model model) {
         List<Material> materialList = materialService.getAllMaterials();
         model.addAttribute("materials", materialList);
         return "materials";
     }
-    @RequestMapping("/material/edit")
+    @GetMapping("/material/edit")
     public String editMaterial(@RequestParam("id") Integer id, Model model) {
         Material material= materialService.getMaterialById(id);
         model.addAttribute("material", material);
         return "materialedit";
     }
 
-    @RequestMapping(value = "/material/save", method = RequestMethod.POST)
+    @PostMapping("/material/save")
     public String saveMaterialChanges(Material material) {
         materialService.saveMaterialChanges(material);
         return "redirect:/materials";
     }
-    @RequestMapping("/material/form")
+    @GetMapping("/material/form")
     public String createNewMaterial(Model model) {
         model.addAttribute("material", new Material());
         return "materialform";
     }
-    @RequestMapping(value = "/materials", method = RequestMethod.POST)
+    @PostMapping("/materials")
     public String saveMaterial(Material material) {
         materialService.saveMaterial(material);
         return "redirect:/materials";
     }
-    @RequestMapping("/material/delete/{id}")
+    @GetMapping("/material/delete/{id}")
     public String deleteMaterial(@PathVariable("id") Integer id) {
         materialService.deleteMaterial(id);
         return "redirect:/materials";
