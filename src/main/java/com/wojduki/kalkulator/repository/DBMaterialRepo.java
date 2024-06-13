22c7ec886636384ceac203b16cf7ded6d3cf662b
wojduki <wojduki@gmail.com>
2024-06-13 10:28:39 +0200

    DB repositories refactor

--- src/main/java/com/wojduki/kalkulator/repository/DBMaterialRepo.java
+++ src/main/java/com/wojduki/kalkulator/repository/DBMaterialRepo.java
@@ -2,33 +2,56 @@ package com.wojduki.kalkulator.repository;
 
 import com.wojduki.kalkulator.model.Material;
 import com.wojduki.kalkulator.model.Type;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import java.util.Collection;
 
 public class DBMaterialRepo implements CostRepo<Material>{
-
+    @PersistenceContext
+    private EntityManager em;
+    @Transactional
     public void createMaterial(String name, String unit, double consumptionPerM2, double unitPrice, Type type) {
+        Material material= new Material(name, unit, consumptionPerM2, unitPrice, type);
+        em.persist(material);
     }
     @Override
+    @Transactional
     public void createCost(Material material){
+        em.persist(material);
     }
     @Override
-    public void updateCost(Integer id,Material material){}
+    @Transactional
+    public void updateCost(Integer id,Material material){
+        em.merge(material);
+    }
 
     @Override
     public Collection<Material> getAllCosts() {
-        return null;
+        return em.createQuery("from Material", Material.class).getResultList();
     }
-
     @Override
     public Material getCostById(Integer id) {
-        return null;
+        return em.createQuery("from Material m where m.id=:id", Material.class)
+                .setParameter("id", id).getSingleResult();
     }
 
     @Override
     public void deleteCost(Integer id) {
+        em.remove(id);
     }
 
     @Override
     public void build() {
+        createMaterial("Grunt", "l", 0.3, 12, Type.FLOOR);
+        createMaterial("Klej", "kg", 2.5, 14, Type.FLOOR);
+        createMaterial("Płytki", "szt.", 4, 40, Type.FLOOR);
+        createMaterial("Grunt", "l", 0.3, 12, Type.WALLS);
+        createMaterial("Farba", "l", 0.2, 15, Type.WALLS);
+        createMaterial("Tapeta", "m.b.", 1, 30, Type.WALLS);
+        createMaterial("Tynk", "worek", 1, 16, Type.WALLS);
+        createMaterial("Grunt", "l", 0.3, 12, Type.CEILING);
+        createMaterial("Gips", "kg", 1.0, 15, Type.CEILING);
+        createMaterial("Farba", "l", 0.2, 15, Type.CEILING);
     }
 }
--- src/main/java/com/wojduki/kalkulator/repository/DBWorkRepo.java
+++ src/main/java/com/wojduki/kalkulator/repository/DBWorkRepo.java
@@ -2,30 +2,43 @@ package com.wojduki.kalkulator.repository;
 
 import com.wojduki.kalkulator.model.Type;
 import com.wojduki.kalkulator.model.Work;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 import java.util.Collection;
 
 public class DBWorkRepo implements CostRepo<Work>{
-
+    @PersistenceContext
+    private EntityManager em;
     public void createWork(String name, double pricePerM2, Type type) {
+        Work work= new Work(name, pricePerM2, type);
+        em.persist(work);
     }
     @Override
     public void createCost(Work work){
+        em.persist(work);
     }
     @Override
-    public void updateCost(Integer id, Work work){}
+    public void updateCost(Integer id, Work work){
+        em.merge(work);
+    }
 
     @Override
     public Collection<Work> getAllCosts() {
-        return null;
+
+        return em.createQuery("from Work", Work.class).getResultList();
     }
 
     @Override
     public Work getCostById(Integer id) {
-        return null;
+
+        return em.createQuery("from Work w where w.id=:id", Work.class)
+                .setParameter("id", id).getSingleResult();
     }
 
     @Override
     public void deleteCost(Integer id) {
+        em.remove(id);
     }
 
     @Override
